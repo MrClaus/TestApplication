a231d5c0c09b74a3f76ef41314af6d38ff85bc66
Marat Kurmaev <girosoft@mail.ru>
2018-04-19 18:04:09 +0300

    - изменение в каталоге "./res"

---
+++ mytestlib/src/main/java/com/example/Student.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Created by gifo on 12.04.2018.
+ */
+
+public class Student {
+    String name = "";
+    public Student(String t) {
+        this.name = t;
+    }
+}

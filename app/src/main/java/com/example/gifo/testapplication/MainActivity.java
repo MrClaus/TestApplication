ba41c43e799b6db78c84d09b4a7c522656bbd22d
Marat Kurmaev <girosoft@mail.ru>
2018-04-19 17:57:35 +0300

    - Пробовал играться с разметкой, освоился с Relative- Linear- Layout's - Добавил разметку выпадающего меню в приложение

--- app/src/main/java/com/example/gifo/testapplication/MainActivity.java
+++ app/src/main/java/com/example/gifo/testapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.gifo.testapplication;
 
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +16,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View view) {
-        ConstraintLayout background = (ConstraintLayout) findViewById(R.id.screen);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Добавляем в приложение Меню из XML-вёрстки(3 вертикальные точки с раскрывающимся списком)
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обрабатываем выбор секций из списка меню
+        int id = item.getItemId();
+        if (id == R.id.action_exit) finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Слушатель событий нажатий на Button
+    public void onClickButton(View view) {
         switch (view.getId()) {
-            case R.id.button_RED:
-                background.setBackgroundColor(getResources().getColor(R.color.colorRed));
+            case R.id.but_plus:
+                setCounterView(1);
                 break;
-            case R.id.button_GREEN:
-                background.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+            case R.id.but_minus:
+                setCounterView(-1);
                 break;
-            case R.id.button_BLUE:
-                background.setBackgroundColor(getResources().getColor(R.color.colorBlue));
+            case R.id.but_order:
+                setPriceView();
                 break;
         }
+    }
+
+    // Метод уменьшения/увеличения значения тега counter при нажатии клавиш тега but_plus/but_minus
+    private void setCounterView(int sign) {
+        int countGet = getCountGet();
+        TextView counterTextView = (TextView) findViewById(R.id.counter);
+        countGet += (sign > 0) ? 1 : (countGet != 0) ? -1 : 0;
+        counterTextView.setText("" + countGet);
+    }
+
+    // Изменение значения тега price
+    private void setPriceView() {
+        int price = 5;
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(getCountGet() * price));
+    }
+
+    // Чтение числового значения тега counter
+    private int getCountGet() {
+        TextView counterTextView = (TextView) findViewById(R.id.counter);
+        String value = "" + counterTextView.getText();
+        try { return Integer.parseInt(value);
+        } catch (Exception e) { return  0; }
     }
 }
--- mytestlib/src/main/java/com/example/MyClass.java
+++ mytestlib/src/main/java/com/example/MyClass.java
@@ -1,4 +1,61 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MyClass {
+    public static void main(String[] args) throws InterruptedException {
+        Cat cat1 = new Cat("Мурка");
+        Cat cat2 = new Cat("Пушинка");
+    }
+
+    private static void investigateWorld() {
+        try { Thread.sleep(200);
+        } catch (Exception e) {}
+    }
+
+    public static class Cat extends Thread {
+        protected Kitten kitten1;
+        protected Kitten kitten2;
+
+        public Cat(String name) {
+            super(name);
+            kitten1 = new Kitten("Котенок 1, мама - " + getName());
+            kitten2 = new Kitten("Котенок 2, мама - " + getName());
+            start();
+        }
+
+        public void run() {
+            System.out.println(getName() + " родила 2 котенка");
+            try {
+                initAllKitten();
+            } catch (InterruptedException e) {
+            }
+            System.out.println(getName() + ": Все котята в корзинке. " + getName() + " собрала их назад");
+        }
+
+        private void initAllKitten() throws InterruptedException {
+            kitten1.start();
+            kitten2.start();
+            kitten1.join();
+            kitten2.join();
+        }
+    }
+
+    public static class Kitten extends Thread {
+        public Kitten(String name) {
+            super(name);
+        }
+
+        public void run() {
+            System.out.println(getName() + ", вылез из корзинки");
+            investigateWorld();
+        }
+    }
 }
